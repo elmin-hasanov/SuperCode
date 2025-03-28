3c34b7997bacc4ce0a38278dc2e1673cd209ae5c
elmin-hasanov <elmin2203@gmail.com>
2025-03-28 16:23:13 +0100

    SuperBank

--- Modul_3_React/06_Day_Exercises_Level-3_1/src/components/SuperBank.tsx
+++ Modul_3_React/06_Day_Exercises_Level-3_1/src/components/SuperBank.tsx
@@ -2,34 +2,42 @@ import { CreditCard } from "react-kawaii";
 import "./SuperBank.css";
 import { useState } from "react";
 
-const mood = [
-  "sad",
-  "shocked",
-  "happy",
-  "blissful",
-  "lovestruck",
-  "excited",
-  "ko",
-];
-
 export default function SuperBank() {
-  const [mood, setMood] = useState("happy");
+  const [balance, setBalance] = useState(0);
+  const [inputVelue, setInputVelue] = useState(0);
+
+  function handleclick(isaddition: boolean) {
+    if (isaddition) {
+      setBalance(inputVelue + balance);
+    } else {
+      setBalance(balance - inputVelue);
+    }
+  }
 
   return (
     <div className="superbank">
       <div className="mood">
-        <CreditCard size={240} mood="blissful" color="#A6E191" />
+        <CreditCard
+          size={240}
+          mood={balance >= 1 ? "happy" : "sad"} // ternary operator
+          color="#A6E191"
+        />
       </div>
-      <div>
-        <h1>SUPER BANK</h1>
-      </div>
-      <div>
+
+      <div className="content">
+        <h1>
+          <span>S</span>UPER <span>B</span>ANK
+        </h1>
+
         <p>Your Bank Account</p>
-        <h2 className="balance">$0</h2>
-        <input type="text" />
-        <div>
-          <button onClick={}>Deposit</button>
-          <button onClick={}>Pay Out</button>
+        <h2 className="balance">{balance}$</h2>
+        <input
+          onChange={(event) => setInputVelue(Number(event.target.value))}
+          type="number"
+        />
+        <div className="buttons">
+          <button onClick={() => handleclick(true)}>Deposit</button>
+          <button onClick={() => handleclick(false)}>Pay Out</button>
         </div>
       </div>
     </div>
