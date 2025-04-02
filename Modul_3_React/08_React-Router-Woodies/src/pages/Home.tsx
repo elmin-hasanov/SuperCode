959a510ba47a463f45ae50b506fca0a4e430721f
elmin-hasanov <elmin2203@gmail.com>
2025-04-03 01:11:55 +0200

    Button fix

--- Modul_3_React/08_React-Router-Woodies/src/pages/Home.tsx
+++ Modul_3_React/08_React-Router-Woodies/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import './Home.css';
 import homeImage from '../assets/img/Home.png';
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <>
@@ -9,7 +10,9 @@ const Home = () => {
             Are you looking for <span>woodden furniture</span> for your place?
           </h1>
           <h2>This is the Right Place</h2>
-          <button>Explore Categories</button>
+          <Link to="/Products">
+            <button>Explore Categories</button>
+          </Link>
         </div>
         <div className="sidebar-content">
           <img className="home-image" src={homeImage} alt="" />
